786b8ef90969068956651613c137577b4ea21c6b
Danielcard99 <dc2651065@gmail.com>
2024-03-19 22:54:36 -0300

    Scanner adicionado em StringInvertida.java

--- src/Lampada.java
+++ src/Lampada.java
@@ -1,9 +1,10 @@
 public class Lampada {
 
   public static void main(String[] args) {
-    // Ligue um dos interruptores e espere um pouco.
-    // Desligue e ligue um segundo interruptor. Vá até a sala.
+    // Primeiro Ligue um dos interruptores e espere um pouco.
+    // Desligue o interruptor e ligue um segundo interruptor. Vá até a sala.
+    // Cheque as lâmpadas.
     // A lâmpada desligada e quente corresponde ao primeiro interruptor,
-    // a lâmpada acesa ao segundo e a lâmpada apagada e fria ao terceiro.
+    // A lâmpada acesa ao segundo e a lâmpada apagada e fria ao terceiro.
   }
 }
--- src/StringInvertida.java
+++ src/StringInvertida.java
@@ -1,12 +1,17 @@
+import java.util.Scanner;
+
 public class StringInvertida {
 
   public static void main(String[] args) {
-    String nome = "TargetSistemas";
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Digite uma Palavra:");
+    String nome = scanner.nextLine();
 
     String nomeInvertido = "";
     for (int i = nome.length() - 1; i >= 0; i--) {
       nomeInvertido = nomeInvertido + nome.charAt(i);
     }
     System.out.println(nomeInvertido);
+    scanner.close();
   }
 }
